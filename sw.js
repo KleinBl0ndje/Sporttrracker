657471caf24f894e9c3c867a93a6dd497ee309ef
KleinBl0ndje <Lisa.de.kort@gmail.com>
2026-08-28 16:44:42 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+// Simpele service worker: cachet de app-schil zodat de app ook opent
+// zonder internet (data ophalen/opslaan blijft uiteraard internet nodig hebben).
+const CACHE_NAME = 'sporttracker-v2';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  // Nooit de Apps Script API cachen, altijd live data ophalen.
+  if (url.hostname.includes('script.google.com')) {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request)
+          .then((response) => {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+            return response;
+          })
+          .catch(() => cached)
+      );
+    })
+  );
+});
